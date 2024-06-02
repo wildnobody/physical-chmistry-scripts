7499d4ebc762fd2de85b8d2b2d8351de50915aa0
Zechari Reisler <zecharireisler@gmail.com>
2024-06-02 23:44:44 +0300

    add significant digits to dataprodes

--- diffusion/dataprodes.m
+++ diffusion/dataprodes.m
@@ -8,12 +8,12 @@ for i = 1: length(txtfiles)
 
     stdl = split(string(txtfiles(i).name), '_');
     m(i,1) = str2double(stdl(3))*3600 + str2double(stdl(4))*60 + str2double(stdl(5));
-    m(i,2) = f.c1/sqrt(2);    
-    m(i,3) = temp(3)/sqrt(2);
-    m(i,4) = f.b1;
-    m(i,5) = temp(2);
-    m(i,6) = f.a1;
-    m(i,7) = temp(1);
+    m(i,2) = round(f.c1/sqrt(2), floor(-log10(temp(3)/sqrt(2))));    
+    m(i,3) = round(temp(3)/sqrt(2), floor(-log10(temp(3)/sqrt(2))));
+    m(i,4) = round(f.b1, floor(-log10(temp(2))));
+    m(i,5) = round(temp(2), floor(-log10(temp(2))));
+    m(i,6) = round(f.c1, floor(-log10(temp(1))));
+    m(i,7) = round(temp(1), floor(-log10(temp(1))));
     
     m(i,8) = g.rsquare;
 
